891d90564c5981ad4dc0fec546a65a3a926d34e0
tmarshall <timothyjmarshall@gmail.com>
2017-01-02 14:17:16 -0800

    testing hosts

--- web/bash/npm/start.sh
+++ web/bash/npm/start.sh
@@ -29,7 +29,7 @@ else
     PIDS[1]=$!;
     # by tracking pids, and using this trap, all tracked processes will be killed after a ^C
     # see http://stackoverflow.com/questions/9023164/in-bash-how-can-i-run-multiple-infinitely-running-commands-and-cancel-them-all
-    trap "kill ${PIDS[*]} && wait ${PIDS[*]} && exit 0 2>/dev/null" SIGINT;
+    trap "kill ${PIDS[*]} && wait ${PIDS[*]} 2>/dev/null" SIGINT;
     wait;
   fi
 fi
