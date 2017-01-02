89a9867d273b989d2f15a84e603386af43ca5382
tmarshall <timothyjmarshall@gmail.com>
2017-01-02 13:07:17 -0800

    little bit of github setup

--- web/server/routes.js
+++ web/server/routes.js
@@ -2,12 +2,46 @@
 
 const config = require('config');
 const express = require('express');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 const log = require('log')('routes');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+/*
+  Logged-out landing page
+ */
+router.get('/', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
   res.render('landing');
 });
+
+/*
+  Dashboard
+ */
+router.get('/', (req, res) => {
+  res.render('dashboard');
+});
+
+/*
+  Auth callback
+ */
+router.get(
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/' // todo: /login ?
+  }),
+  (res, res) => {
+    res.redirect('/');
+  }
+);
+
+/*
+  Auth initiation
+ */
+router.get('/auth/github', passport.authenticate('github'));
 
 module.exports = router;
