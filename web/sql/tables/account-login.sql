1bb7a70289a89fc5346bfe159aeed439fa0810fc
tmarshall <timothyjmarshall@gmail.com>
2017-01-08 18:31:50 -0800

    fix table name duplication

--- web/sql/tables/account-login.sql
+++ web/sql/tables/account-login.sql
@@ -1,6 +1,6 @@
 CREATE TYPE account_login_service AS ENUM('github');
 
-CREATE TABLE account (
+CREATE TABLE account_login (
   id SERIAL PRIMARY KEY,
   account INT REFERENCES account (id) NOT NULL,
   service account_login_service NOT NULL,
