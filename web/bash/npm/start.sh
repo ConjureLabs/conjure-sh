3d3838ebc34aec586466a1f4b543577b17aa38a2
tmarshall <timothyjmarshall@gmail.com>
2017-01-18 19:38:42 -0800

    COSMO -> SENTRY

--- web/bash/npm/start.sh
+++ web/bash/npm/start.sh
@@ -20,33 +20,33 @@ if [ "$CONTAINER" != "docker" ]; then
     fi
 
     mkdir -p $CACHE_DIR/nginx;
-    COSMO_NGINX_CONF_NEEDED=1;
-    COSMO_NGINX_NEW_IP=$(docker-machine ip sentry);
+    SENTRY_NGINX_CONF_NEEDED=1;
+    SENTRY_NGINX_NEW_IP=$(docker-machine ip sentry);
 
     sudo nginx -s stop 2>/dev/null;
 
     if [ -f $CACHE_DIR/nginx/current-ip ]; then
-      COSMO_NGINX_CURRENT_IP=$(cat $CACHE_DIR/nginx/current-ip);
-      if [ "$COSMO_NGINX_NEW_IP" == "$COSMO_NGINX_CURRENT_IP" ]; then
-        COSMO_NGINX_CONF_NEEDED=0;
+      SENTRY_NGINX_CURRENT_IP=$(cat $CACHE_DIR/nginx/current-ip);
+      if [ "$SENTRY_NGINX_NEW_IP" == "$SENTRY_NGINX_CURRENT_IP" ]; then
+        SENTRY_NGINX_CONF_NEEDED=0;
       fi
     fi
 
-    if [ $COSMO_NGINX_CONF_NEEDED == 1 ]; then
+    if [ $SENTRY_NGINX_CONF_NEEDED == 1 ]; then
       progress "Reconfiguring and restarting Nginx";
 
       # always backing up the nginx config
       cp /usr/local/etc/nginx/nginx.conf $CACHE_DIR/nginx/nginx.conf.bk
 
-      COSMO_NGINX_CONF_COUNT=$(cat /usr/local/etc/nginx/nginx.conf | grep "# < COSMO CONF " | wc -l);
-      if [ $COSMO_NGINX_CONF_COUNT == 0 ]; then
+      SENTRY_NGINX_CONF_COUNT=$(cat /usr/local/etc/nginx/nginx.conf | grep "# < SENTRY CONF " | wc -l);
+      if [ $SENTRY_NGINX_CONF_COUNT == 0 ]; then
         # config for this app has not yet been added to nginx
         # doing that now
         # this is assuming the last {} block of the nginx config is for http - may have to update this later
         sed '$s/\}/\
-  # < COSMO CONF START >\
+  # < SENTRY CONF START >\
   \
-  # < COSMO CONF END >\
+  # < SENTRY CONF END >\
   \
   }/' /usr/local/etc/nginx/nginx.conf > $CACHE_DIR/nginx/tmp.conf;
         mv $CACHE_DIR/nginx/tmp.conf /usr/local/etc/nginx/nginx.conf;
@@ -54,14 +54,14 @@ if [ "$CONTAINER" != "docker" ]; then
 
       echo $APP_IP;
 
-      # find COSMO CONF block, and replacing it with the new IP needed
+      # find SENTRY CONF block, and replacing it with the new IP needed
       cp /usr/local/etc/nginx/nginx.conf $CACHE_DIR/nginx/tmp.conf;
-      COSMO_NGINX_CONF_START=$(grep -n '# < COSMO CONF START >' $CACHE_DIR/nginx/tmp.conf | cut -d: -f 1);
-      COSMO_NGINX_CONF_END=$(grep -n '# < COSMO CONF END >' $CACHE_DIR/nginx/tmp.conf | cut -d: -f 1);
+      SENTRY_NGINX_CONF_START=$(grep -n '# < SENTRY CONF START >' $CACHE_DIR/nginx/tmp.conf | cut -d: -f 1);
+      SENTRY_NGINX_CONF_END=$(grep -n '# < SENTRY CONF END >' $CACHE_DIR/nginx/tmp.conf | cut -d: -f 1);
       {
-        head -n $(($COSMO_NGINX_CONF_START-1)) $CACHE_DIR/nginx/tmp.conf
-        sed "s/<APP_IP>/$COSMO_NGINX_NEW_IP/" < $APP_DIR/server/conf/nginx-server-template.conf
-        tail -n $(($(wc -l < $CACHE_DIR/nginx/tmp.conf)-$COSMO_NGINX_CONF_END)) $CACHE_DIR/nginx/tmp.conf
+        head -n $(($SENTRY_NGINX_CONF_START-1)) $CACHE_DIR/nginx/tmp.conf
+        sed "s/<APP_IP>/$SENTRY_NGINX_NEW_IP/" < $APP_DIR/server/conf/nginx-server-template.conf
+        tail -n $(($(wc -l < $CACHE_DIR/nginx/tmp.conf)-$SENTRY_NGINX_CONF_END)) $CACHE_DIR/nginx/tmp.conf
       } > /usr/local/etc/nginx/nginx.conf;
       rm $CACHE_DIR/nginx/tmp.conf;
 
