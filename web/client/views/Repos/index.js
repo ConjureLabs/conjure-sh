bbe69f8bdeb6abf192016d38a2e864f9b13daf72
tmarshall <timothyjmarshall@gmail.com>
2017-04-07 19:56:02 -0700

    listing of repos, better

--- web/client/views/Repos/index.js
+++ web/client/views/Repos/index.js
@@ -1,1 +1,47 @@
-console.log('hi there!');
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, Link } from 'react-router';
+import styles from './styles.styl';
+
+const onRepoClick = Symbol('on repo click');
+
+class Repos extends Component {
+  [onRepoClick](repo) {
+    console.log(repo);
+  }
+
+  render() {
+    return (
+      <div className={styles.root}>
+        {
+          staticContent.repos.map(repo => {
+            return (
+              <a
+                href=''
+                className={styles.repo}
+                onClick={e => {
+                  e.preventDefault();
+                  this[onRepoClick].call(this, repo)
+                }}
+              >
+                {repo.fullName}
+              </a>
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+const routes = [{
+  path: '/',
+  indexRoute: {
+    component: Repos
+  }
+}];
+
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes}/>,
+  document.getElementById('container')
+);
