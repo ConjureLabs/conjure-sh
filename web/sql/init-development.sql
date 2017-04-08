7dc57a2ec525acefa0dec8ab74723a3afe0e4315
tmarshall <timothyjmarshall@gmail.com>
2017-04-08 16:02:05 -0700

    kick voyant connections before db wipe

--- web/sql/init-development.sql
+++ web/sql/init-development.sql
@@ -2,6 +2,10 @@
 
 \c postgres;
 
+-- kick any connections to this database
+SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'voyant' AND pid <> pg_backend_pid();
+
 DROP DATABASE IF EXISTS voyant;
 CREATE DATABASE voyant WITH OWNER voyant_admin;
 \c voyant;
