5426eb4a0458d8987e361d140a3410887f01f27a
tmarshall <timothyjmarshall@gmail.com>
2017-04-13 19:55:52 -0700

    remove test logs

--- web/server/routes/views/get.js
+++ web/server/routes/views/get.js
@@ -9,10 +9,8 @@ const route = new Route();
  */
 route.push((req, res, next) => {
   if (req.isAuthenticated()) {
-    console.log('YES');
     return next();
   }
-  console.log('NO');
 
   res.render('landing', {
     name: 'landing'
