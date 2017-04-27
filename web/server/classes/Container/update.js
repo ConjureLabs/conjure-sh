487aeaff1342a4442fcd712c89ca27ca37d3f3d8
tmarshall <timothyjmarshall@gmail.com>
2017-04-26 20:35:08 -0700

    refactored container update flow

--- web/server/classes/Container/update.js
+++ web/server/classes/Container/update.js
@@ -2,23 +2,15 @@
 
 const log = require('modules/log')('container update');
 
-function containerUpdate(orgName, repoName, payload, callback) {
+function containerUpdate(callback) {
   log.info('starting update');
 
-  const series = [];
-
   // there is a better way to do this - for now, keeping it rather simple
-
-  series.push(cb => {
-    require('./container-destroy')(payload, payload.branch, cb);
-  });
-
-  series.push(cb => {
-    require('./container-create')(orgName, repoName, payload, cb);
-  });
-
   const async = require('async');
-  async.series(series, err => {
+  async.series([
+    this.destroy,
+    this.create
+  ], err => {
     callback(err);
   });
 }
