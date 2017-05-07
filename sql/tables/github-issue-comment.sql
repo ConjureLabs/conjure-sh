41822ff5610fc6b4bdc34e8217dd018b4278afb0
tmarshall <timothyjmarshall@gmail.com>
2017-05-06 21:17:17 -0700

    using comment id correctly

--- sql/tables/github-issue-comment.sql
+++ sql/tables/github-issue-comment.sql
@@ -2,8 +2,10 @@ CREATE TABLE github_issue_comment (
   id SERIAL PRIMARY KEY,
   watched_repo INT REFERENCES watched_repos(id) NOT NULL,
   issue_id INT NOT NULL,
+  comment_id INT NOT NULL,
   url VARCHAR(2000) NOT NULL,
   added TIMESTAMP WITH TIME ZONE NOT NULL,
-  updated TIMESTAMP WITH TIME ZONE
+  updated TIMESTAMP WITH TIME ZONE,
+  UNIQUE(watched_repo, issue_id)
 );
 COMMENT ON TABLE github_issue_comment IS 'tracks github issue comments, posted by conjure';
