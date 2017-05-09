c578da1ec4671bf66c85cc2defabd5ac3411b8d2
tmarshall <timothyjmarshall@gmail.com>
2017-05-08 19:32:33 -0700

    remove single repo view, not used anymore

--- client/views/Repos/index.js
+++ client/views/Repos/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 
 import FullListing from './subviews/FullListing';
-import GitHubRepo from './subviews/GitHubRepo';
 
 const routes = [{
   path: '/r',
   indexRoute: {
     component: FullListing
-  },
-  childRoutes: [{
-    path: '/r/GitHub/:orgName/:repoName',
-    component: GitHubRepo
-  }]
+  }
+  // childRoutes: [{
+  //   path: '/r/GitHub/:orgName/:repoName',
+  //   component: GitHubRepo
+  // }]
 }];
 
 ReactDOM.render(
