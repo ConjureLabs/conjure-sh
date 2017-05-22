ac2568ac7139e9797ca58d71958da7d7cd548538
tmarshall <timothyjmarshall@gmail.com>
2017-05-21 17:50:24 -0700

    figuring out a better redux/reselect/react solution

---
+++ client/modules/MinDux/index.js
@@ -0,0 +1,23 @@
+// import { Component as 'ReactComponent' } from 'react';
+// import { Provider, connect as 'reactReduxConnect' } from 'react-redux';
+// import { createStore as 'reduxCreateStore', applyMiddleware } from 'redux';
+
+// let cachedStore;
+// export function createStore() {
+//   if (cachedStore) {
+//     throw new Error('MiniDux does not allow you to create store twice');
+//   }
+
+//   cachedStore = reduxCreateStore.apply(reduxCreateStore, arguments);
+//   return cachedStore;
+// }
+
+// export Provider;
+// export applyMiddleware;
+
+// export function connect() {
+//   reactReduxConnect
+// }
+
+
+
