61f6787fc069410659f22eae772c5c0457b21a72
tmarshall <timothyjmarshall@gmail.com>
2017-05-26 10:54:27 -0400

    adding cookie session module

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
+const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const log = require('conjure-core/modules/log')();
