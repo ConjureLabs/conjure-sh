3c373fd0663e20c87523be23a131fe21e45d0f14
tmarshall <timothyjmarshall@gmail.com>
2017-06-10 18:59:37 -0700

    work on full express-next-conjure init

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const setup = require('./setup');
 
 const config = require('conjure-core/modules/config');
+const next = require('next');
 const express = require('express');
 const compression = require('compression');
 const morgan = require('morgan');
@@ -10,6 +11,11 @@ const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const log = require('conjure-core/modules/log')();
+
+const app = next({
+  dev: process.env.NODE_ENV !== 'production'
+})
+const nextGetRequestHandler = app.getRequestHandler();
 
 const port = config.app.web.port;
 const server = express();
@@ -66,11 +72,6 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-server.set('views', path.join(__dirname, '..', 'views'));
-server.set('view engine', 'jade');
-server.disable('view cache');
-server.use(express.static(path.resolve(__dirname, '..', 'public')));
-
 server.use((req, res, next) => {
   req.state = {}; // used to track anything useful, along the lifetime of a request
   req.state.remoteAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -81,57 +82,14 @@ server.use((req, res, next) => {
 server.use(setup.routes.views);
 server.use(setup.routes.c);
 
-// 404 status view
-const statusRouter = express.Router();
-statusRouter.get('*', (req, res, next) => {
-  // prevent non-html pages from rendering a 404
-  if (typeof req.headers.accept !== 'string' || !req.headers.accept.includes('text/html')) {
-    return next();
-  }
+server.get('*', (req, res) => {
+  return nextGetRequestHandler(req, res);
+});
 
-  // showing a random confused travolta, for fun
-  const request = require('request');
-
-  request({
-    url: 'http://api.giphy.com/v1/gifs/search',
-    method: 'get',
-    qs: {
-      q: 'confused travolta',
-      api_key: 'dc6zaTOxFJmzC'
-    },
-    json: true
-  }, (err, _, content) => {
-    let gifUrl = `${config.app.web.url}/images/gifs/confused-travolta.gif`;
-    let webpUrl;
-
-    // doing several checks so that 404s do not break becaue of potential gify changes or downtime
-    if (err) {
-      log.error(err);
-    } else {
-      // if we got expected content from the gify endpoint...
-      if (Array.isArray(content.data) && content.data.length) {
-        // attempt to get a random image
-        const index = Math.floor( Math.random() * content.data.length );
-        if (
-          content.data[index].images &&
-          content.data[index].images.original &&
-          typeof content.data[index].images.original.url === 'string'
-        ) {
-          gifUrl = content.data[index].images.original.url;
-          webpUrl = content.data[index].images.original.webp;
-        }
-      }
-    }
-
-    res.render('status-404', {
-      name: 'status-404',
-      gifUrl,
-      webpUrl: webpUrl || ''
+app
+  .prepare()
+  .then(() => {
+    server.listen(port, () => {
+      log.info(`listening on :${port}`);
     });
   });
-});
-server.use(statusRouter);
-
-server.listen(port, () => {
-  log.info(`listening on :${port}`);
-});
