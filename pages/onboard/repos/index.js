fb3cf1da3923512ed7c5bf3cdb99671e27035813
tmarshall <timothyjmarshall@gmail.com>
2017-06-22 20:41:30 -0700

    all onboard pages now use authorization

--- pages/onboard/repos/index.js
+++ pages/onboard/repos/index.js
@@ -1,12 +1,21 @@
 import { Component } from 'react';
 import styles, { classes } from '../styles.js';
+import { ReStore } from '../../../shared/ReStore';
 
 import Header from '../../../components/Header';
 
 export default class OnboardRepos extends Component {
   render() {
+    const { query } = this.props.url;
+
+    const initialState = {
+      account: query.account
+    };
+
     return (
-      <div>
+      <ReStore store={initialState}>
+        <Header />
+
         <div className={classes.wrap}>
           <header>
             <sup>🎟</sup>
@@ -24,7 +33,7 @@ export default class OnboardRepos extends Component {
 
           {styles}
         </div>
-      </div>
+      </ReStore>
     );
   }
 }
---
+++ server/routes/views/onboard/billing/get.js
@@ -0,0 +1,53 @@
+const Route = require('conjure-core/classes/Route');
+const UnexpectedError = require('conjure-core/modules/err').UnexpectedError;
+const nextApp = require('../../../../next');
+
+const route = new Route({
+  requireAuthentication: true,
+  skippedHandler: (req, res) => {
+    nextApp.render(req, res, '/_error');
+  }
+});
+
+route.push((req, res, next) => {
+  const waterfall = [];
+  const DatabaseTable = require('conjure-core/classes/DatabaseTable');
+
+  waterfall.push(callback => {
+    const accountGithub = new DatabaseTable('account_github');
+    accountGithub.select({
+      account: req.user.id
+    }, (err, rows) => {
+      if (err) {
+        return callback(err);
+      }
+
+      // should not be possible
+      if (!rows.length) {
+        return callback(new UnexpectedError('Could not find GitHub account record'));
+      }
+
+      // should not be possible
+      if (rows.length > 1) {
+        return callback(new UnexpectedError('Expected a signle row for GitHub account record, received multiple'));
+      }
+
+      callback(null, rows[0]);
+    });
+  });
+
+  const asyncWaterfall = require('conjure-core/modules/async/waterfall');
+  asyncWaterfall(waterfall, (err, githubAccount) => {
+    if (err) {
+      return next(err);
+    }
+
+    nextApp.render(req, res, '/onboard/billing', {
+      account: {
+        photo: githubAccount.photo
+      }
+    });
+  });
+});
+
+module.exports = route;
