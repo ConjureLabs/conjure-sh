b2aff3997db239f46b91c33acbf77450f3071cd5
tmarshall <timothyjmarshall@gmail.com>
2017-06-24 20:02:45 -0700

    moved <CreditCardInput> to own file

---
+++ components/Input/CreditCard/index.js
@@ -0,0 +1,76 @@
+import Input from '../index.js';
+
+import {
+  cards,
+  cardPrefixDict,
+  maxCardPrefixLength
+} from './cards.js';
+
+export default class CreditCardInput extends Input {
+  constructor(props) {
+    super(props);
+
+    this.type = 'text';
+    this.cardType = undefined;
+  }
+
+  onKeyUp() {
+    super.onChange(...arguments);
+    let prunedValue = this.input.value.replace(/[^\d]/g, '');
+    const cardType = this.determineCard();
+
+    if (cardType === undefined) {
+      if (this.input.value !== prunedValue) {
+        this.input.value = prunedValue;
+      }
+      return;
+    }
+
+    const format = cards[ cardType ].format;
+    const newValueChunks = [];
+    for (let i = 0; i < format.length; i++) {
+      const chunkLen = format[i];
+
+      newValueChunks.push(prunedValue.substr(0, chunkLen));
+      prunedValue = prunedValue.substr(chunkLen);
+
+      if (!prunedValue) {
+        break;
+      }
+    }
+
+    if (
+      // if the card number is not complete
+      newValueChunks.length !== format.length &&
+      // but the last chunk was completed
+      newValueChunks[ newValueChunks.length - 1 ].length === format[ newValueChunks.length - 1 ]
+    ) {
+      // then force another space at the end of the string
+      newValueChunks.push('');
+    }
+
+    const newValue = newValueChunks.join(' ');
+    if (this.input.value !== newValue) {
+      this.input.value = newValue;
+    }
+  }
+
+  determineCard() {
+    const value = this.input.value.replace(/[^\d]/g, '');
+
+    // if the value is too long, we must have already found a match, so more checks would be wasteful
+    if (value.length > maxCardPrefixLength) {
+      return;
+    }
+
+    for (let i = 0; i < value.length; i++) {
+      const currentSubstring = value.substr(0, i + 1);
+      if (cardPrefixDict[currentSubstring]) {
+        this.cardType = cardPrefixDict[currentSubstring];
+        // not breaking loop, so that the longest match is returned
+      }
+    }
+
+    return this.cardType;
+  }
+}
