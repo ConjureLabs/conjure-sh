e373d3c00973e818746572cf98aae9940a963587
tmarshall <timothyjmarshall@gmail.com>
2017-07-07 11:45:53 +0800

    fixed up the table naming

--- sql/tables/account-cards.sql
+++ sql/tables/account-cards.sql
@@ -1,8 +1,8 @@
-CREATE TABLE account_email (
+CREATE TABLE account_cards (
   id SERIAL PRIMARY KEY,
   account INT REFERENCES account (id) NOT NULL,
   stripe_id VARCHAR(40) NOT NULL,
   added TIMESTAMP WITH TIME ZONE NOT NULL,
   UNIQUE (account, stripe_id)
 );
-COMMENT ON TABLE account_email IS 'all known emails associated to an account';
+COMMENT ON TABLE account_email IS 'credit cards associated with an account';
