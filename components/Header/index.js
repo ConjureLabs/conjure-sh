91953c342d60a647bc6f3000e82e6a7563315750
tmarshall <timothyjmarshall@gmail.com>
2017-07-09 14:56:42 -0700

    remove tree nav from header

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,82 +2,13 @@ import { connect } from '../../shared/ReStore';
 import styles, { classes } from './styles.js';
 import classnames from 'classnames';
 
-const Header = ({ account, org, repo, branch, dispatch }) => {
+const Header = ({ account }) => {
   return (
     <header className={classes.root}>
       <span className={classes.wrap}>
-        <span className={classes.treeLocation}>
-          {[
-            <h1
-              className={classnames(classes.treeNav, classes.serviceName, org === null ? classes.current : null)}
-              onClick={org === null ? null : () => {
-                dispatch.selectPlacementInBranchTree({
-                  level: 'none'
-                });
-              }}
-              key='header-content-⎔'
-            >
-              ⎔
-            </h1>,
-
-            org === null ? null : (
-              <h2
-                className={classnames(classes.treeNav, classes.orgName, repo === null ? classes.current : null)}
-                onClick={repo === null ? null : () => {
-                  dispatch.selectPlacementInBranchTree({
-                    level: 'org',
-                    value: org
-                  });
-                }}
-                key='header-content-org'
-              >
-                {org}
-              </h2>
-            ),
-
-            repo === null ? null : (
-              <span
-                className={classes.separator}
-                key='header-content-org-separator'
-              >
-                /
-              </span>
-            ),
-
-            repo === null ? null : (
-              <h3
-                className={classnames(classes.treeNav, classes.repoName, branch === null ? classes.current : null)}
-                onClick={branch === null ? null : () => {
-                  dispatch.selectPlacementInBranchTree({
-                    level: 'repo',
-                    value: repo
-                  });
-                }}
-                key='header-content-repo'
-              >
-                {repo}
-              </h3>
-            ),
-
-            branch === null ? null : (
-              <span
-                className={classes.separator}
-                key='header-content-repo-separator'
-              >
-                /
-              </span>
-            ),
-
-            branch === null ? null : (
-              <h4
-                className={classnames(classes.treeNav, classes.branchName, classes.current)}
-                key='header-content-branch'
-              >
-                {branch}
-              </h4>
-            )
-          ]}
-        </span>
+        <h1 className={classes.serviceName}>
+          <sup>⎔</sup> Conjure
+        </h1>
 
         <nav className={classes.userNav}>
           <span
@@ -116,10 +47,7 @@ const Header = ({ account, org, repo, branch, dispatch }) => {
 
 const selector = store => {
   return {
-    account: store.account,
-    org: store.org,
-    repo: store.repo,
-    branch: store.branch
+    account: store.account
   };
 };
 
