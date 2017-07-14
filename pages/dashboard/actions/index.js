90475db2cc50fe71e725de0cb41c144a6736ce6a
tmarshall <timothyjmarshall@gmail.com>
2017-07-13 19:51:02 -0700

    building up the timeline fetching

--- pages/dashboard/actions/index.js
+++ pages/dashboard/actions/index.js
@@ -1,2 +1,29 @@
+const clearTimeline = store => {
+  return Object.assign({}, store, {
+    timeline: null
+  });
+};
+
+const pushTimeline = (store, { addition }) => {
+  const timeline = (store.timeline || []).slice();
+  timeline.push(...addition);
+
+  return Object.assign({}, store, {
+    timeline
+  });
+};
+
+const unshiftTimeline = (store, { addition }) => {
+  const timeline = store.timeline.slice();
+  timeline.unshift(...addition);
+
+  return Object.assign({}, store, {
+    timeline
+  });
+};
+
 export default {
+  clearTimeline,
+  pushTimeline,
+  unshiftTimeline
 };
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { get } from '../../../shared/xhr';
 import actions from './actions';
 import styles, { classes } from './styles.js';
 import { ReStore } from '../../shared/ReStore';
@@ -6,20 +7,44 @@ import { ReStore } from '../../shared/ReStore';
 import Header from '../../components/Header';
 
 export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    // set at render
+    this.orgDropdown = null;
+  }
+
+  pullTimeline() {
+    post(`${config.app.api.url}/api/org/${this.orgDropdown.value}/containers/timeline`, {
+      page: 0
+    }, (err, data) => {
+      if (err) {
+        console.error(err);
+        alert(err.message);
+        return;
+      }
+
+      actions.pushTimeline({
+        addition: data
+      });
+    });
+  }
+
   render() {
     // todo: avoid using props.url.query
     const { query } = this.props.url;
     const { account, orgs } = query;
 
     const initialState = {
-      account
+      account,
+      timeline: null
     };
 
     return (
       <ReStore store={initialState} actions={actions}>
         <Header>
           <span className={classes.headerContent}>
-            <select>
+            <select ref={this.orgDropdown = ref}>
               {orgs.map(org => {
                 return (
                   <option value={org.id}>{org.login}</option>
