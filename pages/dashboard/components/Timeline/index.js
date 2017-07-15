6c269ea8c6feee550c2588e747816b5f701ec3d1
tmarshall <timothyjmarshall@gmail.com>
2017-07-14 21:57:15 -0700

    force update, so timestamps reflect, every 2 min

--- pages/dashboard/components/Timeline/index.js
+++ pages/dashboard/components/Timeline/index.js
@@ -10,115 +10,126 @@ const minute = 60;
 const hour = 60 * minute;
 const day = 24 * hour;
 
-const Timeline = ({ timeline, org }) => {
-  if (!Array.isArray(timeline)) {
-    return (
-      <div className={classes.loader}>
-        <Loader />
+class Timeline extends Component {
+  componentDidMount() {
+    // forcing timestamps to update every 2 minutes
+    setInterval(() => {
+      this.forceUpdate();
+    }, 2 * 60 * 1000);
+  }
 
-        <span className={classes.text}>hang tight</span>
+  render() {
+    const { timeline, org } = this.props;
+
+    if (!Array.isArray(timeline)) {
+      return (
+        <div className={classes.loader}>
+          <Loader />
+
+          <span className={classes.text}>hang tight</span>
+
+          {styles}
+        </div>
+      );
+    }
+
+    return (
+      <div className={classes.wrap}>
+        {
+          timeline.map(item => {
+            const statusKey = item.status.replace(/\s/g, '').toLowerCase();
+
+            let ms;
+            let duration;
+
+            switch (item.status) {
+              case 'Spinning Up':
+              case 'Running':
+                ms = new Date() - new Date(item.start);
+                break;
+
+              case 'Spun Down':
+                ms = new Date(item.stop) - new Date(item.start);
+                break;
+            }
+
+            if (typeof ms === 'number') {
+              let remainingSeconds = Math.ceil(ms / 1000);
+              let durationDays = Math.floor(remainingSeconds / day);
+              remainingSeconds %= day;
+              let durationHours = Math.floor(remainingSeconds / hour);
+              remainingSeconds %= hour;
+              let durationMinutes = Math.floor(remainingSeconds / minute);
+              let durationSeconds = remainingSeconds % minute;
+
+              if (durationDays === 0 && durationHours === 0 && durationMinutes === 0) {
+                duration = `${durationSeconds} second${durationSeconds === 1 ? '' : 's'}`;
+              } else if (durationDays === 0 && durationHours === 0) {
+                duration = minute / 2 < durationSeconds ? `${durationMinutes + 1} minutes` : `${durationMinutes} minute${durationMinutes === 1 ? '' : 's'}`;
+              } else if (durationDays === 0) {
+                duration = hour / 2 < (durationMinutes * minute) ? `${durationHours + 1} hours` : `${durationHours} hour${durationHours === 1 ? '' : 's'}`;
+              } else {
+                duration = day / 2 < (durationHours * hour) ? `${durationDays + 1} days` : `${durationDays} day${durationDays === 1 ? '' : 's'}`;
+              }
+
+              if (item.status === 'Spun Down') {
+                duration = `Ran ${duration}`;
+              } else {
+                duration = `Up for ${duration}`;
+              }
+            }
+
+            const statusNode = item.status === 'Running' ? (
+              <a
+                href={item.url}
+                target='_blank'
+              >
+                Running
+              </a>
+            ) : item.status;
+
+            return (
+              <ol key={item.id}>
+                <li className={classnames(classes.status, classes[statusKey])}>
+                  <sup />
+                  {statusNode}
+                </li>
+
+                <li className={classnames({
+                  [classes.repo]: true,
+                  [classes.privateRepo]: item.repo_private === true
+                })}>
+                  <sup className={classes.svgIcon} />
+                  <a
+                    href={`https://github.com/${org}/${item.repo}/`}
+                    target='_blank'
+                  >
+                    {item.repo}
+                  </a>
+                </li>
+
+                <li className={classes.branch}>
+                  <sup className={classes.svgIcon} />
+                  <a
+                    href={`https://github.com/${org}/${item.repo}/tree/${item.branch}`}
+                    target='_blank'
+                  >
+                    {item.branch}
+                  </a>
+                </li>
+
+                <li className={classes.duration}>
+                  {duration}
+                </li>
+              </ol>
+            );
+          })
+        }
 
         {styles}
       </div>
     );
   }
-
-  return (
-    <div className={classes.wrap}>
-      {
-        timeline.map(item => {
-          const statusKey = item.status.replace(/\s/g, '').toLowerCase();
-
-          let ms;
-          let duration;
-
-          switch (item.status) {
-            case 'Spinning Up':
-            case 'Running':
-              ms = new Date() - new Date(item.start);
-              break;
-
-            case 'Spun Down':
-              ms = new Date(item.stop) - new Date(item.start);
-              break;
-          }
-
-          if (typeof ms === 'number') {
-            let remainingSeconds = Math.ceil(ms / 1000);
-            let durationDays = Math.floor(remainingSeconds / day);
-            remainingSeconds %= day;
-            let durationHours = Math.floor(remainingSeconds / hour);
-            remainingSeconds %= hour;
-            let durationMinutes = Math.floor(remainingSeconds / minute);
-            let durationSeconds = remainingSeconds % minute;
-
-            if (durationDays === 0 && durationHours === 0 && durationMinutes === 0) {
-              duration = `${durationSeconds} second${durationSeconds === 1 ? '' : 's'}`;
-            } else if (durationDays === 0 && durationHours === 0) {
-              duration = minute / 2 < durationSeconds ? `${durationMinutes + 1} minutes` : `${durationMinutes} minute${durationMinutes === 1 ? '' : 's'}`;
-            } else if (durationDays === 0) {
-              duration = hour / 2 < (durationMinutes * minute) ? `${durationHours + 1} hours` : `${durationHours} hour${durationHours === 1 ? '' : 's'}`;
-            } else {
-              duration = day / 2 < (durationHours * hour) ? `${durationDays + 1} days` : `${durationDays} day${durationDays === 1 ? '' : 's'}`;
-            }
-
-            if (item.status === 'Spun Down') {
-              duration = `Ran ${duration}`;
-            } else {
-              duration = `Up for ${duration}`;
-            }
-          }
-
-          const statusNode = item.status === 'Running' ? (
-            <a
-              href={item.url}
-              target='_blank'
-            >
-              Running
-            </a>
-          ) : item.status;
-
-          return (
-            <ol key={item.id}>
-              <li className={classnames(classes.status, classes[statusKey])}>
-                <sup />
-                {statusNode}
-              </li>
-
-              <li className={classnames({
-                [classes.repo]: true,
-                [classes.privateRepo]: item.repo_private === true
-              })}>
-                <sup className={classes.svgIcon} />
-                <a
-                  href={`https://github.com/${org}/${item.repo}/`}
-                  target='_blank'
-                >
-                  {item.repo}
-                </a>
-              </li>
-
-              <li className={classes.branch}>
-                <sup className={classes.svgIcon} />
-                <a
-                  href={`https://github.com/${org}/${item.repo}/tree/${item.branch}`}
-                  target='_blank'
-                >
-                  {item.branch}
-                </a>
-              </li>
-
-              <li className={classes.duration}>
-                {duration}
-              </li>
-            </ol>
-          );
-        })
-      }
-
-      {styles}
-    </div>
-  );
 };
 
 const selector = store => {
