61fffcc7f901bc62f1242321851d0a850f8a81e2
tmarshall <timothyjmarshall@gmail.com>
2017-07-14 19:35:00 -0700

    durations, human readable

--- pages/dashboard/components/Timeline/index.js
+++ pages/dashboard/components/Timeline/index.js
@@ -4,6 +4,11 @@ import { ReStore, connect } from '../../../../shared/ReStore';
 
 import Loader from '../../../../components/Loader';
 
+// in seconds
+const minute = 60;
+const hour = 60 * minute;
+const day = 24 * hour;
+
 const Timeline = ({ timeline }) => {
   if (!Array.isArray(timeline)) {
     return (
@@ -21,11 +26,53 @@ const Timeline = ({ timeline }) => {
     <div className={classes.wrap}>
       {
         timeline.map(item => {
+          const statusKey = item.status.replace(/\s/g, '').toLowerCase();
+
+          let ms;
+          let duration;
+
+          switch (item.status) {
+            case 'Spinning Up':
+            case 'Running':
+              ms = new Date() - new Date(item.start);
+              break;
+
+            case 'Spun Down':
+              ms = new Date(item.stop) - new Date(item.start);
+              break;
+          }
+
+          if (typeof ms === 'number') {
+            let remainingSeconds = Math.ceil(ms / 1000);
+            let durationDays = Math.floor(remainingSeconds / day);
+            remainingSeconds %= day;
+            let durationHours = Math.floor(remainingSeconds / hour);
+            remainingSeconds %= hour;
+            let durationMinutes = Math.floor(remainingSeconds / minute);
+            let durationSeconds = remainingSeconds % minute;
+
+            if (durationDays === durationHours === durationMinutes === 0) {
+              duration = `${durationSeconds} second${durationSeconds === 1 ? '' : 's'}`;
+            } else if (durationDays === durationHours === 0) {
+              duration = minute / 2 < durationSeconds ? `${durationMinutes + 1} minutes` : `${durationMinutes} minute${durationMinutes === 1 ? '' : 's'}`;
+            } else if (durationDays === 0) {
+              duration = hour / 2 < (durationMinutes * minute) ? `${durationHours + 1} hours` : `${durationHours} hour${durationHours === 1 ? '' : 's'}`;
+            } else {
+              duration = day / 2 < (durationHours * hour) ? `${durationDays + 1} days` : `${durationDays} day${durationDays === 1 ? '' : 's'}`;
+            }
+
+            if (item.status === 'Spun Down') {
+              duration = `Ran ${duration}`;
+            } else {
+              duration = `Up for ${duration}`;
+            }
+          }
+
           return (
-            <ol>
-              <li>{item.status}</li>
+            <ol key={item.id}>
+              <li className={classes[statusKey]}>{item.status}</li>
               <li>{item.branch}</li>
-              <li>{item.start}</li>
+              <li>{duration}</li>
             </ol>
           );
         })
