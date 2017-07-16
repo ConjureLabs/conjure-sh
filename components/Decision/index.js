cc8ca60ea28b3f260852987cd3f896efd7308582
tmarshall <timothyjmarshall@gmail.com>
2017-07-15 18:07:39 -0700

    +<Decision>

---
+++ components/Decision/index.js
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react';
+import styles, { classes } from './styles.js';
+import classnames from 'classnames';
+
+import Button from '../Button';
+
+export default class Decision extends Component {
+  handleClick(type) {
+    const { onPrimaryClick, onSecondaryClick, disabled } = this.props;
+
+    if (disabled) {
+      return;
+    }
+
+    const handler = type === 'primary' ? onPrimaryClick :
+      type === 'secondary' ? onSecondaryClick :
+      null;
+
+    if (handler) {
+      handler(new Event('Button Clicked'));
+    }
+  }
+
+  render() {
+    const { className, secondaryText, hallow, disabled } = this.props;
+
+    const rootClasses = classnames(
+      classes.root,
+      className
+    );
+
+    return (
+      <span className={rootClasses}>
+        <Button
+          className={classes.secondary}
+          onClick={this.handleClick.bind(this, 'secondary')}
+          disabled={disabled === true}
+          hallow={hallow === true}
+        >
+          {secondaryText}
+        </Button>
+
+        <Button
+          className={classes.primary}
+          onClick={this.handleClick.bind(this, 'primary')}
+          disabled={disabled === true}
+          hallow={hallow === true}
+        >
+          {primaryText}
+        </Button>
+      </span>
+    );
+  }
+}
