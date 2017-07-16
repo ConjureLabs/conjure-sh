aee9707d868bd39def6ec111e96dc7501e11572a
tmarshall <timothyjmarshall@gmail.com>
2017-07-16 16:40:38 -0700

    remove null constraints

--- sql/tables/container.sql
+++ sql/tables/container.sql
@@ -3,12 +3,12 @@ CREATE TABLE container (
   repo INT REFERENCES watched_repo(id),
   branch VARCHAR(255) NOT NULL, -- 255 is max github length
   -- commit_sha VARCHAR(40) NOT NULL,
-  domain VARCHAR(200) NOT NULL,
-  port INT NOT NULL,
-  container_id VARCHAR(64) NOT NULL,
+  domain VARCHAR(200),
+  port INT,
+  container_id VARCHAR(64),
   url_uid VARCHAR(24) NOT NULL,
   is_active BOOLEAN NOT NULL,
-  active_start TIMESTAMP WITH TIME ZONE NOT NULL,
+  active_start TIMESTAMP WITH TIME ZONE,
   active_stop TIMESTAMP WITH TIME ZONE,
   added TIMESTAMP WITH TIME ZONE NOT NULL,
   updated TIMESTAMP WITH TIME ZONE
