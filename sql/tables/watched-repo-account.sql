03a943f5aa990c30c40617ac03fbbe3409862ad3
tmarshall <timothyjmarshall@gmail.com>
2017-08-13 20:15:16 -0700

    change table to only track watched repos that are visible by account

---
+++ sql/tables/watched-repo-account.sql
@@ -0,0 +1,9 @@
+CREATE TABLE watched_repo_account (
+  id SERIAL PRIMARY KEY,
+  account INT REFERENCES account(id) NOT NULL,
+  watched_repo INT REFERENCES watched_repo(id) NOT NULL,
+  added TIMESTAMP WITH TIME ZONE NOT NULL,
+  updated TIMESTAMP WITH TIME ZONE,
+  UNIQUE (service, account, watched_repo)
+);
+COMMENT ON TABLE watched_repo_account IS 'watched conjure repos visible by accounts';
