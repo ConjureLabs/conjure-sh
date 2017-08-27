a6a6c1ffc930c899e5405584e2447c853e0a56c2
tmarshall <timothyjmarshall@gmail.com>
2017-08-27 16:10:06 -0700

    card entry bar

--- pages/account/billing/index.js
+++ pages/account/billing/index.js
@@ -3,6 +3,7 @@ import styles, { classes } from './styles.js';
 import { ReStore } from '../../../shared/ReStore';
 
 import Header from '../../../components/Header';
+import Button from '../../../components/Button';
 import CreditCardSummary from '../../../components/CreditCardSummary';
 
 export default class AccountBilling extends Component {
@@ -17,6 +18,21 @@ export default class AccountBilling extends Component {
     return (
       <ReStore store={initialState}>
         <Header />
+
+        <div className={classes.buttonWrap}>
+          <div className={classes.wrap}>
+            <Button
+              color='blue'
+              hallow={false}
+              size='small'
+              onClick={() => {
+                window.location='/account/billing/entry';
+              }}
+            >
+              Add New Card
+            </Button>
+          </div>
+        </div>
 
         <div className={classes.wrap}>
           {cards.map(card => {
