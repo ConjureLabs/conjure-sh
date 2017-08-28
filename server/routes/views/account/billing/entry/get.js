d96c83423e3d85ef4951273a600d8bd23554fc0d
tmarshall <timothyjmarshall@gmail.com>
2017-08-27 17:44:26 -0700

    limit logic on billing entry view getter

--- server/routes/views/account/billing/entry/get.js
+++ server/routes/views/account/billing/entry/get.js
@@ -1,6 +1,5 @@
 const Route = require('conjure-core/classes/Route');
 const nextApp = require('../../../../../next');
-const UnexpectedError = require('conjure-core/modules/err').UnexpectedError;
 
 const route = new Route({
   requireAuthentication: true
@@ -10,40 +9,15 @@ const route = new Route({
   Repos listing
  */
 route.push((req, res, next) => {
-  const async = require('async');
-
-  const parallel = {};
-
-  parallel.github = callback => {
-    const apiGetAccountGitHub = require('conjure-api/server/routes/api/account/github/get.js').call;
-    apiGetAccountGitHub(req, null, (err, result) => {
-      if (err) {
-        return callback(err);
-      }
-
-      callback(null, result.account);
-    });
-  };
-
-  parallel.cards = callback => {
-    const apiGetAccountCards = require('conjure-api/server/routes/api/account/cards/get.js').call;
-    apiGetAccountCards(req, null, (err, result) => {
-      if (err) {
-        return callback(err);
-      }
-
-      callback(null, result.cards);
-    });
-  };
-
-  async.parallel(parallel, (err, records) => {
+  const apiGetAccountGitHub = require('conjure-api/server/routes/api/account/github/get.js').call;
+  apiGetAccountGitHub(req, null, (err, result) => {
     if (err) {
       return next(err);
     }
 
     nextApp.render(req, res, '/account/billing/entry', {
       account: {
-        photo: records.github.photo // todo: not rely on github...
+        photo: result.account.photo // todo: not rely on github...
       }
     });
   });
