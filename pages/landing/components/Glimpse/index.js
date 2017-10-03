36fdac13f990f900629d1c34a0a71391b410f6b7
tmarshall <timothyjmarshall@gmail.com>
2017-10-02 19:43:35 -0700

    <Glimpse> filling in

--- pages/landing/components/Glimpse/index.js
+++ pages/landing/components/Glimpse/index.js
@@ -3,7 +3,19 @@ import styles, { classes } from './styles.js';
 export default () => {
   return (
     <div className={classes.root}>
-      
+      <div className={classes.toolbar}>
+        <ins />
+        <ins />
+        <ins />
+      </div>
+
+      <div className={classes.content}>
+        <span className={classes.title}>New Sidebar</span>
+        <ins>Open</ins>
+
+        <span className={classes.author} />
+      </div>
+
       {styles}
     </div>
   );
