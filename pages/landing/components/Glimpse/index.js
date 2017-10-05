644ba6eb4739dfd497da1d3f0d6ed02a256435ff
tmarshall <timothyjmarshall@gmail.com>
2017-10-04 20:01:46 -0700

    element loading more reliable

--- pages/landing/components/Glimpse/index.js
+++ pages/landing/components/Glimpse/index.js
@@ -12,23 +12,31 @@ export default class Glimpse extends Component {
       inViewport: false // once true, will not be set back
     };
 
-    if (typeof window !== 'undefined') {
-      window.addEventListener('scroll', this.checkIfInViewport.bind(this));
+    if (typeof window !== 'undefined' && window.addEventListener) {
+      const boundHandler = this.checkIfInViewport.bind(this);
+
+      window.addEventListener('scroll', boundHandler);
+      window.addEventListener('resize', boundHandler);
+
+      this.removeBoundHandler = () => {
+        window.removeEventListener('scroll', boundHandler);
+        window.removeEventListener('resize', boundHandler);
+      };
+    } else {
+      // if can't add listeners, then just force state
+      this.setState({
+        inViewport: true
+      });
     }
   }
 
   checkIfInViewport() {
-    if (!this.conjureContent) {
+    if (!this.conjureContent || this.state.inViewport) {
       return;
     }
 
     const rect = this.conjureContent.getBoundingClientRect();
     const html = document.documentElement;
-
-    console.log('yes?', rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || html.clientHeight) &&
-        rect.right <= (window.innerWidth || html.clientWidth));
 
     if (
       !(
@@ -41,10 +49,12 @@ export default class Glimpse extends Component {
       return;
     }
 
-    window.removeEventListener('scroll', this.checkIfInViewport);
-
     this.setState({
       inViewport: true
+    }, () => {
+      if (this.removeBoundHandler) {
+        this.removeBoundHandler();
+      }
     });
   }
 
@@ -54,7 +64,10 @@ export default class Glimpse extends Component {
     return (
       <div
         className={classes.root}
-        ref={ref => this.conjureContent = ref}
+        ref={ref => {
+          this.conjureContent = ref;
+          this.checkIfInViewport();
+        }}
       >
         <div className={classes.toolbar}>
           <ins />
