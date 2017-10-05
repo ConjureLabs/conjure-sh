923eae22c74515714e225347700c7c61b32123be
tmarshall <timothyjmarshall@gmail.com>
2017-10-04 18:52:41 -0700

    working on viewport teaser

--- pages/landing/components/Glimpse/index.js
+++ pages/landing/components/Glimpse/index.js
@@ -13,7 +13,19 @@ export default () => {
         <span className={classes.title}>New Sidebar</span>
         <ins>Open</ins>
 
-        <span className={classes.author} />
+        <span className={classes.commentWrap}>
+          <span className={classes.author} />
+
+          <span className={classes.comment}>
+            <div className={classes.header}>
+              <span className={classes.handle}>tmarshall</span>
+
+              commented on Oct 4
+            </div>
+
+            <span className={classes.body}>This is some text and more text</span>
+          </span>
+        </span>
       </div>
 
       {styles}
