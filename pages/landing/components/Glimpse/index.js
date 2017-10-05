48057766e0f10fe694d411e15c89236e7d3cfe7f
tmarshall <timothyjmarshall@gmail.com>
2017-10-04 19:52:36 -0700

    stylish <Glimpse>!

--- pages/landing/components/Glimpse/index.js
+++ pages/landing/components/Glimpse/index.js
@@ -1,68 +1,125 @@
+import { Component } from 'react';
 import styles, { classes } from './styles.js';
 import classnames from 'classnames';
 
-export default () => {
-  return (
-    <div className={classes.root}>
-      <div className={classes.toolbar}>
-        <ins />
-        <ins />
-        <ins />
-      </div>
+export default class Glimpse extends Component {
+  constructor(props) {
+    super(props);
 
-      <div className={classes.content}>
-        <span className={classes.title}>New Sidebar</span>
-        <ins>Open</ins>
+    this.conjureContent = null; // component ref, set via render
 
-        <span className={classes.commentWrap}>
-          <span className={classes.author} />
+    this.state = {
+      inViewport: false // once true, will not be set back
+    };
 
-          <span className={classes.comment}>
-            <div className={classes.header}>
-              <span className={classes.handle}>tmarshall</span>
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', this.checkIfInViewport.bind(this));
+    }
+  }
 
-              commented one minute ago
-            </div>
+  checkIfInViewport() {
+    if (!this.conjureContent) {
+      return;
+    }
 
-            <span className={classes.body}>Updated the sidebar so that it's easier for users to get to settings</span>
-          </span>
-        </span>
+    const rect = this.conjureContent.getBoundingClientRect();
+    const html = document.documentElement;
 
-        <ol className={classes.timeline}>
-          <li>
+    console.log('yes?', rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || html.clientHeight) &&
+        rect.right <= (window.innerWidth || html.clientWidth));
+
+    if (
+      !(
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || html.clientHeight) &&
+        rect.right <= (window.innerWidth || html.clientWidth)
+      )
+    ) {
+      return;
+    }
+
+    window.removeEventListener('scroll', this.checkIfInViewport);
+
+    this.setState({
+      inViewport: true
+    });
+  }
+
+  render() {
+    const { inViewport } = this.state;
+
+    return (
+      <div
+        className={classes.root}
+        ref={ref => this.conjureContent = ref}
+      >
+        <div className={classes.toolbar}>
+          <ins />
+          <ins />
+          <ins />
+        </div>
+
+        <div className={classes.content}>
+          <span className={classes.title}>New Sidebar</span>
+          <ins>Open</ins>
+
+          <span className={classes.commentWrap}>
             <span className={classes.author} />
-            <span className={classes.commit}>added new dropdown</span>
-          </li>
-          <li>
-            <span className={classes.author} />
-            <span className={classes.commit}>styled sidebar changes</span>
-          </li>
-          <li>
-            <span className={classes.author} />
-            <span className={classes.commit}>tweaks from design</span>
-          </li>
-          <li>
-            <span className={classes.author} />
-            <span className={classes.commit}>changed some label text</span>
-          </li>
-        </ol>
 
-        <span className={classes.commentWrap}>
-          <span className={classnames(classes.comment, classes.fromConjure)}>
-            <div className={classes.header}>
-              <span className={classes.handle}>Conjure</span>
+            <span className={classes.comment}>
+              <div className={classes.header}>
+                <span className={classes.handle}>tmarshall</span>
 
-              commented just now
-            </div>
+                commented one minute ago
+              </div>
 
-            <span className={classes.body}>
-              <span className={classes.simulateLink}>You can view this branch on Conjure</span>
+              <span className={classes.body}>Updated the sidebar so that it's easier for users to get to settings</span>
             </span>
           </span>
-        </span>
-      </div>
 
-      {styles}
-    </div>
-  );
+          <ol className={classes.timeline}>
+            <li>
+              <span className={classes.author} />
+              <span className={classes.commit}>added new dropdown</span>
+            </li>
+            <li>
+              <span className={classes.author} />
+              <span className={classes.commit}>styled sidebar changes</span>
+            </li>
+            <li>
+              <span className={classes.author} />
+              <span className={classes.commit}>tweaks from design</span>
+            </li>
+            <li>
+              <span className={classes.author} />
+              <span className={classes.commit}>changed some label text</span>
+            </li>
+          </ol>
+
+          <span className={classes.commentWrap}>
+            <span
+              className={classnames(classes.comment, classes.fromConjure, {
+                [classes.inViewport]: inViewport
+              })}
+            >
+              <div className={classes.header}>
+                <span className={classes.handle}>Conjure</span>
+
+                commented just now
+              </div>
+
+              <span className={classes.body}>
+                <span className={classes.simulateLink}>You can view this branch on Conjure</span>
+              </span>
+            </span>
+          </span>
+        </div>
+
+        {styles}
+      </div>
+    );
+  }
 };
