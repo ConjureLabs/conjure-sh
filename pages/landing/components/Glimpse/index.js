c667f6ab4ccae3190e8e3314c36ad6b425f9519e
tmarshall <timothyjmarshall@gmail.com>
2017-10-04 19:35:23 -0700

    <Glimpse /> is very close, now

--- pages/landing/components/Glimpse/index.js
+++ pages/landing/components/Glimpse/index.js
@@ -1,4 +1,5 @@
 import styles, { classes } from './styles.js';
+import classnames from 'classnames';
 
 export default () => {
   return (
@@ -20,10 +21,43 @@ export default () => {
             <div className={classes.header}>
               <span className={classes.handle}>tmarshall</span>
 
-              commented on Oct 4
+              commented one minute ago
             </div>
 
             <span className={classes.body}>Updated the sidebar so that it's easier for users to get to settings</span>
+          </span>
+        </span>
+
+        <ol className={classes.timeline}>
+          <li>
+            <span className={classes.author} />
+            <span className={classes.commit}>added new dropdown</span>
+          </li>
+          <li>
+            <span className={classes.author} />
+            <span className={classes.commit}>styled sidebar changes</span>
+          </li>
+          <li>
+            <span className={classes.author} />
+            <span className={classes.commit}>tweaks from design</span>
+          </li>
+          <li>
+            <span className={classes.author} />
+            <span className={classes.commit}>changed some label text</span>
+          </li>
+        </ol>
+
+        <span className={classes.commentWrap}>
+          <span className={classnames(classes.comment, classes.fromConjure)}>
+            <div className={classes.header}>
+              <span className={classes.handle}>Conjure</span>
+
+              commented just now
+            </div>
+
+            <span className={classes.body}>
+              <span className={classes.simulateLink}>You can view this branch on Conjure</span>
+            </span>
           </span>
         </span>
       </div>
