8a9050a0aa92089fb5d67733ea238a19dc689fe4
tmarshall <timothyjmarshall@gmail.com>
2017-11-04 14:44:40 -0700

    simpler lint

--- bash/yarn/lint.sh
+++ bash/yarn/lint.sh
@@ -4,26 +4,18 @@ BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 . $BASE/../functions.cfg;
 
 if ! hash jscs 2>/dev/null; then
-  error "Could not run jscs - you may need to run 'yarn global add jscs'";
+  error "Could not run jscs - you may need to run 'npm install jscs -g'";
   exit 1;
 fi
 
 if ! hash eslint 2>/dev/null; then
-  error "Could not run eslint - you may need to run 'yarn global add eslint'";
+  error "Could not run eslint - you may need to run 'npm install eslint -g'";
   exit 1;
 fi
 
-# todo: fix global check, with yarn ('yarn global ls' is not working)
-# BABEL_LINT=$(yarn list -g | grep babel-eslint | wc -l);
-
-# if [ $BABEL_LINT -eq 0 ]; then
-#   error "eslint is installed, but babel support is not - you may need to run 'yarn global add babel-eslint'";
-#   exit 1;
-# fi
-
 set -e;
 
-eslint ./**/*.js;
+eslint ./**/*.js --quiet;
 jscs ./**/*.js;
 
 progress "Lint passed";
