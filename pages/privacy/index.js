c913153e96ff903a01202b52da99b65e49c5d465
tmarshall <timothyjmarshall@gmail.com>
2017-11-12 16:44:02 -0800

    privacy UI fixes

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -1,6 +1,6 @@
 import styles, { classes } from './styles.js';
 
-export default => (
+export default () => (
   <div className={classes.root}>
     <header className={classes.header}>
       <a href='/'>
@@ -8,13 +8,13 @@ export default => (
       </a>
     </header>
 
-    <main>
+    <main className={classes.wrap}>
       <h2>Conjure Privacy Policy</h2>
 
-      <strong>This Privacy Policy is effective as of November 12, 2017.</strong>
+      <span>This Privacy Policy is effective as of November 12, 2017.</span>
 
       <p>Conjure Labs, Inc. dba Conjure, a Delaware corporation ("Conjure", "we" or "us") provides this Privacy Policy to inform users of our policies and procedures regarding the collection, use and disclosure of personally identifying information (or "PII") received from users of our website at https://conjure.sh (the "Website") and our software-as-a-service platform (the "Service"). This Privacy Policy applies to the Website and the Service, and includes information collected via the Website, email, SMS, telephone or other means.</p>
-      <p>By using or accessing the Website or the Service, you are accepting the practices described in this Privacy Policy, and you are consenting to our processing of your information as set forth in this Privacy Policy now and as amended by us. If you have any questions or comments about this Privacy Policy or our use of your personally identifying information, please contact us at info@conjure.sh.</p>
+      <p>By using or accessing the Website or the Service, you are accepting the practices described in this Privacy Policy, and you are consenting to our processing of your information as set forth in this Privacy Policy now and as amended by us. If you have any questions or comments about this Privacy Policy or our use of your personally identifying information, please contact us at <a href='mailto:info@conjure.sh'>info@conjure.sh</a>.</p>
       <p>Except as otherwise expressly set forth in this Privacy Policy or otherwise mutually agreed with the applicable customer, our use of PII collected through the Service shall be limited to the purpose of providing the Service for which the applicable customer has engaged Conjure.</p>
 
       <h3>Service Users and Website Visitors</h3>
@@ -22,11 +22,11 @@ export default => (
       <p>Conjure also collects potentially personally identifying information like Internet Protocol ('IP') addresses. Conjure does not use such information to indentify users, other than for providing the Service and support purposes, and does not disclose such information other than under the circumstances described below.</p>
 
       <h3>Gathering of Personally Identifying Information</h3>
-      <p>Certain visistors to the Website and Service choose to interact with Conjure in ways that require Conjure to gather PII. The type and amount of information gathered depends on the nature of the interaction. For example, when signing up with the Service, we may ask a User to enter an email address, associated company name, or other information, which can be used to contact the user. Each user may be required to provide a username and password, along with other information, that Conjure uses to create and administer accounts. Conjure may collect such information from third parties, such as GitHub, after the user authenticates with through a third party and grants Conjure access. In each case, Conjure collects such information only as necessary or appropriate to fulfill the purpose of the user's interaction with Conjure. Therefore, a user's PII may be obtained by Conjure by a user logging into the Website or Service using a sign-in service, such as OAuth (e.g. a 'Sign-In with GitHub'), as more fully described below.</p>
+      <p>Certain visistors to the Website and Service choose to interact with Conjure in ways that require Conjure to gather PII. The type and amount of information gathered depends on the nature of the interaction. For example, when signing up with the Service, we may ask a User to enter an email address, associated company name, or other information, which can be used to contact the user. Each user may be required to provide a username and password, along with other information, that Conjure uses to create and administer accounts. Conjure may collect such information from third parties, such as GitHub, after the user authenticates with through a third party and grants Conjure access. In each case, Conjure collects such information only as necessary or appropriate to fulfill the purpose of the user's interaction with Conjure. Therefore, a user's PII may be obtained by Conjure by a user logging into the Website or Service using a sign-in service, such as OAuth (e.g. Sign-In with GitHub), as more fully described below.</p>
       <p>Conjure does not disclose PII other than described in this Privacy Policy. Visitors are always allowed to refuse to supply PII, with the understanding that that may prevent Conjure from being able to engage with the visitor, and the Website and Service may not be accessible.</p>
 
       <h3>Access to Personally Identifying Information</h3>
-      <p>If your PII changes, or if you no longer want to access the Website or Service, you may contact Conjure at info@conjure.sh to request that PII is deleted, ameneded or altered. Conjure will respond in a reasonable timeframe.</p>
+      <p>If your PII changes, or if you no longer want to access the Website or Service, you may contact Conjure at <a href='mailto:info@conjure.sh'>info@conjure.sh</a> to request that PII is deleted, ameneded or altered. Conjure will respond in a reasonable timeframe.</p>
       <p>If the customer for the Service is an organization, and a user is accessing and using the Service as an authorized representitive of that organization, references to 'you' and the like in this Privacy Policy generally refer to the customer organization, not individual end users. In such cases, Conjure receives and processes information under the direction of its customer and has no direct contractual relationship with the individuals whose PII is or may be provided. Any individual in such an organization, who seeks to delete, amend or alter data, should direct their request ot the applicable Conjure customer and we will respond to the Customer's request in a reasonable timeframe.</p>
     
       <h3>Data Retention</h3>
@@ -45,6 +45,18 @@ export default => (
       <p>A cookie is a string of information that a website stores on a visitor's computer, and that the visitor's browser provides to the website each time the visitor returns. Conjure uses cookies to help identify and track visitors, their usage of Conjure services, and their preferences. Conjure visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using Conjure's services, with the drawback that certain or all of the features of Conjure's services may not function properly without the aid of cookies.</p>
       <p>Certain of our third-party providers employ clear gifs (also known as 'beacons' and 'bugs'), images and scripts that help them better manage content. We do not tie the information gathered to our Customers' or users' personally identifying information.</p>
       <p>Conjure or third parties with whom we partner to provide certain features on our Website or Service may also use Flash cookies and browser-based local storage to collect and store information. Various browsers may offer their own management tools for removing browser-based local storage.</p>
+
+      <h3>Links to Other Websites</h3>
+      <p>This Privacy Policy applies only to the Website and the Service and not to any third-party sites or hosted services you may find or access through our Website. If you submit personally identifying information to any of those sites or services, your information is governed by their privacy policies.</p>
+
+      <h3>Testimonials</h3>
+      <p>Conjure may post testimonials, of satisified customers, on the Website. With your consent we may post your testimonial along with your name. If you wish to update or delete your testimonial, you can contact us at <a href='mailto:info@conjure.sh'>info@conjure.sh</a>.</p>
+
+      <h3>Social Media Widgets</h3>
+      <p>Conjure may include social media widgets, such as a Twitter button, or Facebook share button, or any other interactive component. These features may collect your IP address, which page you are visiting on the Website, and may set a cookie to enable the feature to function properly. Social media features and widgets are either hosted by a third party or hosted directly on the Websites or the Service. Your interactions with these features are governed by the privacy policy of the company providing it.</p>
+
+      <h3>Single Sign-On</h3>
+      <p>You can log in to the Website and the Service using sign-in services such as OAuth (e.g. Sign-In with GitHub). These services will authenticate your identity and provide you the option to share certain personally identifying information with Conjure, such as your name and email address.</p> 
     </main>
 
     <footer className={classes.footer}>
