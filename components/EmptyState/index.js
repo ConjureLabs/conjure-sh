dfea079be88ecdaebac08b8387dbda6d1061b692
tmarshall <timothyjmarshall@gmail.com>
2017-11-19 16:58:26 -0800

    simplified arrow method components

--- components/EmptyState/index.js
+++ components/EmptyState/index.js
@@ -2,19 +2,17 @@ import { Component } from 'react';
 import styles, { classes } from './styles.js';
 import classnames from 'classnames';
 
-export default ({ emoji, headerText, bodyText, className }) => {
-  return (
-    <div className={classnames(classes.root, className)}>
-      <div className={classes.header}>
-        <sup>{emoji}</sup>
-        <span>{headerText}</span>
-      </div>
-
-      <span className={classes.body}>
-        {bodyText}
-      </span>
-
-      {styles}
+export default ({ emoji, headerText, bodyText, className }) => (
+  <div className={classnames(classes.root, className)}>
+    <div className={classes.header}>
+      <sup>{emoji}</sup>
+      <span>{headerText}</span>
     </div>
-  );
-};
+
+    <span className={classes.body}>
+      {bodyText}
+    </span>
+
+    {styles}
+  </div>
+);
--- components/Loader/index.js
+++ components/Loader/index.js
@@ -1,12 +1,10 @@
 import styles, { classes } from './styles.js';
 import classnames from 'classnames';
 
-export default ({ className }) => {
-  return (
-    <span className={classnames(classes.wrap, className)}>
-      <span className={classes.loader}>
-        {styles}
-      </span>
+export default ({ className }) => (
+  <span className={classnames(classes.wrap, className)}>
+    <span className={classes.loader}>
+      {styles}
     </span>
-  );
-};
+  </span>
+);
