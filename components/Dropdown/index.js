5a7054b85f3b962acca44299799bc3cb2374e79d
tmarshall <timothyjmarshall@gmail.com>
2017-11-26 12:10:01 -0800

    udpates to <Dropdown/>

--- components/Dropdown/index.js
+++ components/Dropdown/index.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import ClickOutside from 'react-click-outside';
 
 import styles, { classes } from './styles';
 
@@ -7,9 +9,24 @@ export default class Dropdown extends Component {
   constructor(props) {
     super(props);
 
+    const { value, options } = props;
+
+    const selectedOption = typeof value === undefined ? null : options.find(opt => opt.value === value);
+
     this.state = {
-      open: false
+      open: false,
+      selectedOption: selectedOption
     };
+  }
+
+  get value() {
+    const { selectedOption } = this.state;
+
+    if (!selectedOption) {
+      return null;
+    }
+
+    return selectedOption.value;
   }
 
   toggleOpen() {
@@ -18,33 +35,90 @@ export default class Dropdown extends Component {
     });
   }
 
-  get value() {
-    return this.props.options[0].value;
+  close() {
+    this.setState({
+      open: false
+    });
   }
 
   render() {
-    const { placeholder, options } = this.props;
+    const { placeholder, options, className } = this.props;
     const { open } = this.state;
 
     return (
-      <div
+      <ClickOutside
         className={classes.root}
-        onClick={this.toggleOpen.bind(this)}
+        onClickOutside={this.close.bind(this)}
       >
-        <span className={classes.placeholder}>{placeholder || 'Select a value'}</span>
+        <div
+          className={classnames(classes.dropdown, className)}
+          onClick={this.toggleOpen.bind(this)}
+        >
+          <span className={classes.placeholder}>{placeholder} ▼</span>
 
-        {!open ? null : (
-          <div className={classes.options}>
-          {options.map(option => (
-            <span className={classnames(classes.option, option.className)}>
-              {option.label}
-            </span>
-          ))}
-          </div>
-        )}
+          {!open ? null : (
+            <div className={classes.options}>
+            {options.map(option => (
+              <span className={classnames(classes.option, option.className)}>
+                {option.label}
+              </span>
+            ))}
+            </div>
+          )}
 
-        {styles}
-      </div>
+
+          {styles}
+        </div>
+
+      </ClickOutside>
     );
+  }
+
+  static propTypes = {
+    placeholder: PropTypes.string.isRequired,
+
+    options: props => {
+      const { options } = props;
+
+      if (!Array.isArray(options)) {
+        return new Error('Dropdown options must be an array');
+      }
+
+      if (!options.length) {
+        return new Error('Dropdown must have at least one option');
+      }
+
+      const validOptions = options.every(opt => {
+        return typeof opt.label === 'string' &&
+          (
+            typeof opt.value === 'string' ||
+            typeof opt.value === 'number'
+          );
+      });
+
+      if (!validOptions) {
+        return new Error('Dropdown options are missing necessary attributes');
+      }
+
+      const uniqueValues = options.reduce((values, opt) => {
+        if (!values.includes(opt.value)) {
+          values.push(opt.value);
+        }
+        return values;
+      }, []);
+
+      if (uniqueValues.length !== options.length) {
+        return new Error('Dropdown options must have unique values');
+      }
+    },
+
+    value: PropTypes.oneOf([
+      PropTypes.string,
+      PropTypes.number
+    ])
+  }
+
+  static defaultProps = {
+    placeholder: 'Select a value'
   }
 }
