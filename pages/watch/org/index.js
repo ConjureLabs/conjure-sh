75874da4eaeab4c22e384e24e555f9be01cea5ce
tmarshall <timothyjmarshall@gmail.com>
2017-12-03 15:45:22 -0800

    /watch/org view

--- pages/watch/org/index.js
+++ pages/watch/org/index.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import styles, { classes } from '../styles.js';
+import styles, { classes } from './styles.js';
 import { post } from '../../../shared/xhr';
 import config from '../../../shared/config.js';
 
@@ -9,7 +9,7 @@ import AnchorList from '../../../components/AnchorList';
 
 let submitting = false;
 
-export default class OnboardOrgs extends Component {
+export default class WatchOrg extends Component {
   makeSelection(item) {
     // {label: "ConjureLabs", value: 1783213}
     if (submitting) {
@@ -18,7 +18,7 @@ export default class OnboardOrgs extends Component {
 
     submitting = true;
 
-    post(`${config.app.api.url}/api/onboard/orgs/selection`, item, err => {
+    post(`${config.app.api.url}/api/org/${item.value}/watch`, {}, err => {
       if (err) {
         console.error(err);
         alert(err.message);
@@ -26,30 +26,28 @@ export default class OnboardOrgs extends Component {
         return;
       }
 
-      window.location = '/onboard/plan';
+      window.location = `/watch/repos?org=${item.value}`;
     });
   }
 
   render() {
     const { url } = this.props;
     const { query } = url;
+    const { orgs, watchedOrgs } = query;
+
+    const listedOrgs = orgs.filter(org => !watchedOrgs.includes(org.login));
 
     return (
       <Layout url={url} limitedHeader={true}>
         <div className={classes.content}>
           <header>
-            <sup>👋</sup>
-            <span>Welcome to Conjure! Let's get started.</span>
-          </header>
-
-          <article>
-            <sup>1</sup>
+            <sup>🚀</sup>
             <span>Select the GitHub organization or account you'd like to use with Conjure</span>
-          </article>
+          </header>
 
           <main>
             <AnchorList
-              list={query.orgs.map(org => {
+              list={listedOrgs.map(org => {
                 return {
                   label: org.login,
                   value: org.id,
