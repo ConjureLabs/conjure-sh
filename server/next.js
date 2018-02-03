1b2f871e1f707a65af6fdf44e4d520630797ec54
tmarshall <timothyjmarshall@gmail.com>
2018-02-03 15:25:38 -0800

    remove unused const

--- server/next.js
+++ server/next.js
@@ -1,5 +1,4 @@
 // using this file to cache the next app in one export
-const Route = require('route');
 const next = require('next');
 const app = next({
   dev: process.env.NODE_ENV !== 'production'
