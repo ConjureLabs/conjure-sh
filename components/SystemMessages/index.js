4a7c0dda5d9dc1d44db89156c733f33d648e2d71
tmarshall <timothyjmarshall@gmail.com>
2018-04-12 08:35:51 -0700

    diff colors

--- components/SystemMessages/index.js
+++ components/SystemMessages/index.js
@@ -1,10 +1,15 @@
 import { Component } from 'react'
 import Portal from '../Portal'
+import classnames from 'classnames'
 import styles, { classes } from './styles'
 import messageStyles, { classes as messageClasses } from './styles.message'
 
-const Message = ({ children }) => (
-  <article className={messageClasses.root}>
+const Message = ({ children, success = false, error = false }) => (
+  <article className={classnames({
+    [messageClasses.root]: true,
+    [messageClasses.success]: success,
+    [messageClasses.error]: error
+  })}>
     <span>{children}</span>
     {messageStyles}
   </article>
@@ -15,8 +20,8 @@ export default class SystemMessages extends Component {
     return (
       <Portal>
         <div className={classes.root}>
-          <Message>asdf</Message>
-          <Message>asdf</Message>
+          <Message success>You totally did everything you need to do, so just sit back, relax, and enjoy the show! Rock on! CONJURE CONJURE CONJURE!!!</Message>
+          <Message error>asdf</Message>
           <Message>asdf</Message>
           {styles}
         </div>
