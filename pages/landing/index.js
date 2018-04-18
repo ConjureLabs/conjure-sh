15e3b7956b6b9fb3e72a6c8df97a82f663283e6f
tmarshall <timothyjmarshall@gmail.com>
2018-04-17 19:39:01 -0700

    -Easily

--- pages/landing/index.js
+++ pages/landing/index.js
@@ -58,7 +58,7 @@ export default class Landing extends Component {
           <div className={classes.ctaContainer}>
             <div className={classes.firstImpression}>
               Ephemeral Staging Deployments
-              <p>Easily sync your GitHub projects with Conjure and start viewing in minutes!</p>
+              <p>Connect your GitHub projects with Conjure and start viewing in minutes!</p>
             </div>
 
             <div>
