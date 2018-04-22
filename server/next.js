e82fa5c3c4ba1093e13a0ae185fb8a9969cff3ca
tmarshall <timothyjmarshall@gmail.com>
2018-04-21 17:58:19 -0700

    set conf

--- server/next.js
+++ server/next.js
@@ -1,7 +1,10 @@
 // using this file to cache the next app in one export
 const next = require('next')
 const app = next({
-  dev: process.env.NODE_ENV !== 'production'
+  dev: process.env.NODE_ENV !== 'production',
+  conf: {
+    useFileSystemPublicRoutes: false
+  }
 })
 
 module.exports = app
