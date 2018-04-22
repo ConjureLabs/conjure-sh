80347bc2695269a6f9287804852a5d2e238c65ff
tmarshall <timothyjmarshall@gmail.com>
2018-04-21 17:58:22 -0700

    remove pages

--- pages/docs/configuration/language-versions/java.js
+++
@@ -1,7 +0,0 @@
-export default [
-  { suppress: false, content: 'oracle-8' },
-  { suppress: false, content: 'oracle-7' },
-  { suppress: false, content: 'oracle-6' },
-  { suppress: false, content: 'openjdk7' },
-  { suppress: false, content: 'openjdk6' }
-]
