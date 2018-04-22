7c056b4147670e99be855b0d8e7f58f2dc7c15dc
tmarshall <timothyjmarshall@gmail.com>
2018-04-21 19:19:00 -0700

    fix up profile setting

--- bash/yarn/build.sh
+++ bash/yarn/build.sh
@@ -3,7 +3,9 @@
 BASE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 . $BASE/../functions.cfg;
 
-source $BASH_DIR/apply-profile.sh;
+set -e
+
+source $APP_DIR/.profile;
 
 node $APP_DIR/scripts/stylus/prepare.js;
 node $APP_DIR/scripts/config/generate-client-config.js;
