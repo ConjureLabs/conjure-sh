988e1bf27584c52cb1abfcdd4a71bbc0f5c7b556
tmarshall <timothyjmarshall@gmail.com>
2018-04-24 18:42:52 -0700

    updated client config

--- .hob/config.js
+++ .hob/config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  serverConfigRequire: 'conjure-core/modules/config',
-
-  clientConfigKeys: [
-    'app.api.url',
-    'app.web.url',
-    'app.worker.port',
-    'app.worker.protocol'
-  ]
+  clientConfig: {
+    fullConfig: require('conjure-core/modules/config'),
+    keys: [
+      'app.api.url',
+      'app.web.url',
+      'app.worker.port',
+      'app.worker.protocol'
+    ]
+  }
 }
--- client-config.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable */
-// jscs:disable
-
-export default {"app":{"api":{"url":"http://conjure.test:2999"},"web":{"url":"http://conjure.test:3000"}}}
