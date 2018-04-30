60d98a7435d046ef155ba2c238edb7cd99910840
tmarshall <timothyjmarshall@gmail.com>
2018-04-29 18:04:24 -0700

    use new pricing

--- sql/tables/monthly-billing-plan.sql
+++ sql/tables/monthly-billing-plan.sql
@@ -1,14 +1,11 @@
 CREATE TABLE monthly_billing_plan (
   id SERIAL PRIMARY KEY,
-  cost INT NOT NULL,
-  parallel_container_limit INT NOT NULL,
+  container_flat_fee_tenths_cent INT NOT NULL,
+  container_hour_running_fee_tenths_cent INT NOT NULL,
   added TIMESTAMP WITH TIME ZONE NOT NULL,
   activated TIMESTAMP WITH TIME ZONE,
   deactivated TIMESTAMP WITH TIME ZONE
 );
 COMMENT ON TABLE monthly_billing_plan IS 'lookup table for active and past monthly billing plans';
 
-INSERT INTO monthly_billing_plan(cost, parallel_container_limit, added, activated) VALUES(50, 1, NOW(), NOW());
-INSERT INTO monthly_billing_plan(cost, parallel_container_limit, added, activated) VALUES(200, 4, NOW(), NOW());
-INSERT INTO monthly_billing_plan(cost, parallel_container_limit, added, activated) VALUES(500, 10, NOW(), NOW());
-INSERT INTO monthly_billing_plan(cost, parallel_container_limit, added, activated) VALUES(900, 20, NOW(), NOW());
+INSERT INTO monthly_billing_plan(container_flat_fee_tenths_cent, container_hour_running_fee_tenths_cent, added, activated) VALUES(500, 25, NOW(), NOW());
