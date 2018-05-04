ed935ee269b9751b365c0451a322c20e685db637
tmarshall <timothyjmarshall@gmail.com>
2018-05-03 18:37:04 -0700

    plan 1 is free (internal)

--- sql/tables/monthly-billing-plan.sql
+++ sql/tables/monthly-billing-plan.sql
@@ -8,4 +8,5 @@ CREATE TABLE monthly_billing_plan (
 );
 COMMENT ON TABLE monthly_billing_plan IS 'lookup table for active and past monthly billing plans';
 
+INSERT INTO monthly_billing_plan(container_flat_fee_tenths_cent, container_hour_running_fee_tenths_cent, added, activated) VALUES(0, 0, NOW(), NOW());
 INSERT INTO monthly_billing_plan(container_flat_fee_tenths_cent, container_hour_running_fee_tenths_cent, added, activated) VALUES(500, 25, NOW(), NOW());
