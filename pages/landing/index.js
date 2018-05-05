3fd2950495c7ff883e19f07688f214f9ffa3897b
tmarshall <timothyjmarshall@gmail.com>
2018-05-05 15:53:20 -0700

    split out header into own component

--- pages/landing/index.js
+++ pages/landing/index.js
@@ -3,8 +3,7 @@ import styles, { classes } from './styles.js'
 import classnames from 'classnames'
 
 import Layout from '../../components/Layout'
-import Button from '../../components/Button'
-import config from '../../client.config.js'
+import LandingHeader from './components/LandingHeader'
 
 export default class Landing extends Component {
   constructor(props) {
@@ -27,52 +26,7 @@ export default class Landing extends Component {
         withWrapper={false}
         className={classes.root}
       >
-        <form
-          action={`${config.app.api.url}/auth/github`}
-          className={classes.trueForm}
-          method='post'
-          ref={form => this.form = form}
-        />
-
-        <header className={classes.header}>
-          <nav>
-            <h1>Conjure</h1>
-            <a
-              href=''
-              onClick={this.submitForm}
-            >
-              Sign In
-            </a>
-          </nav>
-
-          <div className={classes.ctaContainer}>
-            <div className={classes.whatIsConjure}>
-              <h2>Ephemeral Staging Deployments</h2>
-              <p>Connect your GitHub projects and start viewing in minutes</p>
-            </div>
-
-            <div className={classes.buttons}>
-              <Button
-                size='large'
-                className={classes.cta}
-                color='white'
-                hallow={false}
-                onClick={this.submitForm}
-              >
-                <span className={classes.label}>Sign Up</span>
-              </Button>
-
-              <Button
-                size='large'
-                color='white'
-                hallow={true}
-                href='/docs'
-              >
-                <span className={classes.label}>Explore the Docs</span>
-              </Button>
-            </div>
-          </div>
-        </header>
+        <LandingHeader/>
 
         {styles}
       </Layout>
