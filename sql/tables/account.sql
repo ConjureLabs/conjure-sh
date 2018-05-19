b95da363426355843bb734cc009aa541f3edf9ab
tmarshall <timothyjmarshall@gmail.com>
2018-05-18 19:30:48 -0700

    do not need extra column

--- sql/tables/account.sql
+++ sql/tables/account.sql
@@ -4,7 +4,6 @@ CREATE TABLE account (
   email VARCHAR(255) UNIQUE,
   stripe_id VARCHAR(40) UNIQUE,
   onboarded BOOLEAN,
-  requires_installation BOOLEAN DEFAULT FALSE,
   added TIMESTAMP WITH TIME ZONE NOT NULL,
   updated TIMESTAMP WITH TIME ZONE
 );
