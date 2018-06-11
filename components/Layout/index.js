f39c0a576627c268947bec19c257077d7bcc6023
tmarshall <timothyjmarshall@gmail.com>
2018-06-10 17:56:15 -0700

    fix style attr

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -85,7 +85,7 @@ export default class Layout extends Component {
           { process.env.NODE_ENV === 'production' ? <script async src='https://www.googletagmanager.com/gtag/js?id=UA-108457027-1' /> : null }
           { process.env.NODE_ENV === 'production' ? <script dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)} gtag('js', new Date()); gtag('config', 'UA-108457027-1');` }} /> : null }
           { process.env.NODE_ENV === 'production' ? <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0'; n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)}(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '179588329357608'); fbq('track', 'PageView');` }} /> : null }
-          { process.env.NODE_ENV === 'production' ? (<noscript><img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id=179588329357608&ev=PageView&noscript=1' /></noscript>) : null }
+          { process.env.NODE_ENV === 'production' ? (<noscript><img height='1' width='1' style={{ display: 'none' }} src='https://www.facebook.com/tr?id=179588329357608&ev=PageView&noscript=1' /></noscript>) : null }
           <title>{title}</title>
           <meta name='description' content='Ephemeral Staging Deployments for your GitHub Pull Requests' />
           <meta name='og:description' content={config.app.web.url} />
