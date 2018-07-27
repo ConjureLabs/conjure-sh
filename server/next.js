c374c83c0cd47c5b46a32a13d32c3c59f217da68
tmarshall <tim@marshall.io>
2018-07-27 17:36:56 +0700

    fixed next resolves

--- server/next.js
+++ server/next.js
@@ -1,9 +1,11 @@
 // using this file to cache the next app in one export
 const next = require('next')
+const path = require('path')
 const remarkHighlight = require('remark-highlight.js')
 
 const app = next({
   dev: process.env.NODE_ENV !== 'production',
+  publicRuntimeConfig: require('../client.config.js'),
   conf: {
     useFileSystemPublicRoutes: false,
     pageExtensions: ['js', 'md'],
@@ -20,6 +22,12 @@ const app = next({
           }
         ]
       })
+
+      config.resolve = config.resolve || {}
+      config.resolve.alias = config.resolve.alias || {}
+      config.resolve.alias.components = path.resolve(__dirname, '..', 'components')
+      config.resolve.alias.shared = path.resolve(__dirname, '..', 'shared')
+      config.resolve.alias['client.config.js'] = path.resolve(__dirname, '..', 'client.config.js')
 
       return config
     }
